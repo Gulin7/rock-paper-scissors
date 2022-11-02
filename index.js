5583a1045d3125086c735ef807fda7610faf65d9
Gulin7 <gulintudor@gmail.com>
2022-11-02 14:12:07 +0200

    UPDATE

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
+
 // POSSIBLE CHOICES AND SCORE VARIABLES
 const choices = ["rock", "paper", "scissors"];
 let playerScore = 0, computerScore = 0;
 
 // SCORE DIV
-const scoreDiv = document.querySelector('.score');
-const scoreH1 = document.createElement('h1');
-scoreDiv.appendChild(scoreH1);
-scoreH1.innerText = `Player ${playerScore} vs ${computerScore} Computer`;
+const scoreDiv = document.querySelector('.scoreboard');
+const score = document.querySelector('.score');
+const outcome = document.querySelector('.outcome');
 
 // BUTTONS 
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorsButton = document.querySelector('#scissors');
-
-// OUTCOME DIV
-const outcomeDiv = document.querySelector('.outcome');
-const p = document.createElement('p');
-outcomeDiv.appendChild(p)
 
 function getComputerChoice() {
       // Returns a random choice from the choice-array we created at the top of the doc
@@ -30,37 +25,60 @@ function checkWinner(playerSelection, computerSelection) {
             (playerSelection == "scissors" && computerSelection == "paper") ||
             (playerSelection == "paper" && computerSelection == "rock"))
             return "Player";
-      else return "Computer"; 
+      else return "Computer";
       // if the player didn't win then the computer won
 }
 
 function playRound(playerSelection, computerSelection) {
       const result = checkWinner(playerSelection, computerSelection);
       if (result == "Tie") {
-            p.innerText = "It's a Tie!";  
+            outcome.innerText = "It's a Tie!";
       }
       else if (result == "Player") {
             playerScore++;
-            p.innerText = `You win!: ${playerSelection} beats ${computerSelection}! :D`;
+            outcome.innerText = `You win... ${playerSelection} beats ${computerSelection} :D`;
       }
       else {
             computerScore++;
-            p.innerText = `You lose!: ${computerSelection} beats ${playerSelection}! :(`;
+            outcome.innerText = `You lose... ${computerSelection} beats ${playerSelection} :(`;
       }
       if (computerScore == 5) {
-            scoreH1.innerText = 'The computer won! :( \n'+
-                  `Player: ${playerScore} vs ${computerScore} Computer`;
+            outcome.innerText = 'The computer won :(';
+            swal({
+                  text: 'The computer won :(',
+                  confirmButtonText: 'GG',
+                  confirmButtonColor: '#3085d6',
+                  background: '#fff url(/images/trees.png)',
+                  backdrop: `
+                    rgba(0,0,123,0.4)
+                    url("/images/nyan-cat.gif")
+                    left top
+                    no-repeat
+                  `      
+            });
+            score.innerText = `Player ${playerScore} vs ${computerScore} Computer`;
             computerScore = 0;
             playerScore = 0;
       }
       else if (playerScore == 5) {
-            scoreH1.innerText = 'You won! Congratulations!\n'+
-            `Player: ${playerScore} vs ${computerScore} Computer`;
+            score.innerText = `Player ${playerScore} vs ${computerScore} Computer`;
+            swal({
+                  text: 'YOU WON!!!',
+                  confirmButtonText: 'GG',
+                  confirmButtonColor: '#3085d6',
+                  background: '#fff url(/images/mountain.jpg)',
+                  backdrop: `
+                    rgba(0,0,123,0.4)
+                    url("/images/victory-royale.gif")
+                    left top
+                    no-repeat
+                  `
+            });
             computerScore = 0;
             playerScore = 0;
       }
       else {
-            scoreH1.innerText = `Player ${playerScore} vs ${computerScore} Computer`;
+            score.innerText = `Player ${playerScore} vs ${computerScore} Computer`;
       }
 }
 
@@ -96,34 +114,3 @@ scissorsButton.addEventListener('click', () => {
       playRound(playerSelection, computerSelection);
 });
 
-/*function game() {
-      console.log("Welcome!");
-      let playerScore = 0, computerScore = 0;
-      let score = `Player: ${playerScore} vs Computer:$ {computerScore}`;
-      // console.log(score);
-      for (let i = 0; i < 5; i++) {
-            const playerSelection = getPlayerChoice();
-            const computerSelection = getComputerChoice();
-            if (checkWinner(playerSelection, computerSelection) == "Player") {
-                  playerScore++;
-            }
-            else if (checkWinner(playerSelection, computerSelection) == "Computer") {
-                  computerScore++;
-            }
-            score = `Player: ${playerScore} vs Computer: ${computerScore}`;
-            console.log(score);
-            console.log(playRound(playerSelection, computerSelection));
-            console.log("-------------------------")
-      }
-      if (playerScore > computerScore) {
-            console.log('You win!');
-      }
-      else if (playerScore < computerScore) {
-            console.log('You lose!');
-      }
-      else {
-            console.log('It\'s a tie!');
-      }
-}
-
-game();*/
